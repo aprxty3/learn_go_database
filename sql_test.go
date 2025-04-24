4f7308c28c2da9717797d83470ef1f5f9eadf916
aji prasetyo <ajiprxty3@gmail.com>
2025-04-24 10:31:52 +0700

    query context

--- sql_test.go
+++ sql_test.go
@@ -19,3 +19,30 @@ func TestExecSql(t *testing.T) {
 	}
 	fmt.Println("Success insert data")
 }
+
+func TestQuerySql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+	script := "select * from customer"
+
+	rows, err := db.QueryContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, name string
+		err := rows.Scan(&id, &name)
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("id:", id, "name:", name)
+
+	}
+}
